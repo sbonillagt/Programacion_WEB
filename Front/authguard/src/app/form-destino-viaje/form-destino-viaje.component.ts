cdce953cdb720677c556114523ee1a10a90f1701
Sebastian Bonilla <sbonilla@correo.url.edu.gt>
2020-09-15 22:28:59 -0600

    Ordenando Formulario y validaciones
    Ordenando formulario y validaciones

--- Front/authguard/src/app/form-destino-viaje/form-destino-viaje.component.ts
+++ Front/authguard/src/app/form-destino-viaje/form-destino-viaje.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
+import { MatDatepickerInputEvent } from '@angular/material/datepicker';
+import { DestinoViaje } from '../models/destino-viaje';
 
 @Component({
   selector: 'app-form-destino-viaje',
@@ -6,10 +9,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form-destino-viaje.component.scss']
 })
 export class FormDestinoViajeComponent implements OnInit {
+@Output() onItemAdded: EventEmitter<DestinoViaje>;
 
-  constructor() { }
+formGroupFormulario: FormGroup;
+
+  constructor(formBuilderFormulario: FormBuilder) {
+    this.onItemAdded = new EventEmitter();
+    this.formGroupFormulario = formBuilderFormulario.group({
+      iNombreOrganizador: ['', Validators.required],
+      iPersonas: [''],
+      iDestino: [''],
+      fechaSeleccionada: ['',Validators.required],
+      iDescripcion: [''],
+    });
+   }
+
+  guardar(iNombreOrganizador:string, iPersonas:number, iDestino:string, fechaSeleccionada:Date, iDescripcion:string):boolean{
+    let destino = new DestinoViaje(iNombreOrganizador,iPersonas,iDestino,fechaSeleccionada,iDescripcion);
+    this.onItemAdded.emit(destino)
+    return false;
+  }
 
   ngOnInit(): void {
+  }
+
+  fechaSeleccionada: Date;
+  range = new FormGroup({
+    start: new FormControl(),
+    end: new FormControl()
+  });
+  
+  panelOpenState = false;
+  step = 0;
+
+  setStep(index: number) {
+    this.step = index;
+  }
+
+  nextStep() {
+    this.step++;
+  }
+
+  prevStep() {
+    this.step--;
   }
 
 }
--- Front/authguard/src/app/lista-destinos/lista-destinos.component.ts
+++ Front/authguard/src/app/lista-destinos/lista-destinos.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import {DestinoViaje} from 'src/app/models/destino-viaje';
-import { MatDatepickerInputEvent } from '@angular/material/datepicker';
-import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
+
+
 
 @Component({
   selector: 'app-lista-destinos',
@@ -9,54 +9,28 @@ import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
   styleUrls: ['./lista-destinos.component.scss']
 })
 export class ListaDestinosComponent implements OnInit {
-  fechaSeleccionada: Date;
-  range = new FormGroup({
-    start: new FormControl(),
-    end: new FormControl()
-  });
-
-  addEvent(type: string, event: MatDatepickerInputEvent<Date>) {
-
-  }
-
+  @Output() onItemAdded: EventEmitter<DestinoViaje>;
+  
   destinos: DestinoViaje[] = [];
 
   constructor() { 
-  }
-
-  idate: string = '';
-  guardar(iNombreOrganizador:string, ipersonas:number, iDestino:string,ifecha:Date,idescripcion:string): boolean {
-    
- 
-    this.destinos.push(new DestinoViaje(iNombreOrganizador,ipersonas,iDestino,ifecha,idescripcion));
-    console.log(this.destinos);
-
-    return false;
-  }
-  
-
-  elegido(d:DestinoViaje){
-    this.destinos.forEach(function (x){x.setSelected(false);});
-    d.setSelected(true);
+    this.onItemAdded = new EventEmitter();
   }
 
   ngOnInit(): void {
 
   }
 
-  panelOpenState = false;
-  step = 0;
-
-  setStep(index: number) {
-    this.step = index;
+  idate: string = '';
+  agregado(destino: DestinoViaje) { 
+    console.log("Entro A AGREGADO")
+    console.log(destino)
+    this.destinos.push(destino);
+    this.onItemAdded.emit(destino);
   }
 
-  nextStep() {
-    this.step++;
+  elegido(destino:DestinoViaje){
+    this.destinos.forEach(function (x){x.setSelected(false);});
+    destino.setSelected(true);
   }
-
-  prevStep() {
-    this.step--;
-  }
-
 }
